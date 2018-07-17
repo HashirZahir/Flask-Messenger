5b9f45a9f98919aa2733952204d01a25203d034e
Hashir Zahir <hashirsahad@gmail.com>
2018-07-17 22:24:54 +0800

    added user login and logout functionalities

--- models.py
+++ models.py
@@ -1,20 +1,48 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import LoginManager, UserMixin
 
-# create an instance of SQLAlchemy to pass on to run.py
+# create an instance of SQLAlchemy and LoginManager to pass on to run.py
 db = SQLAlchemy()
+login_manager = LoginManager()
+login.login_view = 'login'
+
+
+@login_manager.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 # database schema for users
-class User(db.Model):
+class User(UserMixin, db.Model):
 
-    # table name in database
     __tablename__ = "user"
 
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
     password_hash = db.Column(db.String(128))
+    messages = db.Relationship('Post', backref='author', lazy='dynamic')
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
     # function thats called when object is prointed
     # useful for debugging
     def __repr__(self):
-        return '<User {}>'.format(self.username)   
+        return '<User: {}>'.format(self.username)   
+
+class Post(db.Model):
+
+    __tablename__ = "post"
+
+    id = db.Column(db.Integer, primary_key=True)
+    message_text = db.Column(db.String(1000))
+    time_stamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Message: {}>'.format(self.message_text) 
--- run.py
+++ run.py
@@ -1,22 +1,22 @@
-from flask import Flask, render_template, flash, redirect
+from flask import Flask, render_template, flash, redirect, request, abort
 from config import Config
 import forms
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from models import db
+from models import db, User, login_manager
+from flask_login import current_user, login_user, logout_user
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 app.config.from_object(Config)
 db.init_app(app)
 # migrate = Migrate(app, db)
+login_manager.init_app(app)
 
 @app.route('/index')
+@app.route('/')
+@login_required
 def index():
     return render_template('index.html')
-
-@app.route('/')
-def home():
-    return redirect(url_for('index'))
 
 @app.route('/signup')
 def signup():
@@ -24,13 +24,32 @@ def signup():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    # if a logged in user tries to go to /login
+    # redirect the user to index
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
     login_form = forms.LoginForm()
     if login_form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            login_form.username.data, login_form.remember_me.data))
-        return redirect(url_for('index'))
+
+        user = User.query.filter_by(username=login_form.username.data).first()
+        if not user.check_password(login_form.password.data) or user is None:
+            flash("Incorrect Username or Password")
+            return redirect(url_for('login'))
+
+        login_user(user,remember = login_form.remember_me.data)
+        flash('Logged in successfully.')
+        next_page = request.args.get('next')
+        # is_safe_url should check if the url is safe for redirects.
+        if not is_safe_url(next_page):
+            return abort(400, "Unsafe URL")
+        return redirect(url_for('/index'))
     return render_template('login.html', login_form = login_form)
 
+@app.route('/logout'):
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
 
 
 if __name__ == '__main__':
