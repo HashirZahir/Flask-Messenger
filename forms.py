0aea8a55a2507d3e6539b4cbbdea99eeae010ed3
Hashir Zahir <hashirsahad@gmail.com>
2018-07-18 12:22:29 +0800

    added sign up functionality

--- forms.py
+++ forms.py
@@ -1,9 +1,30 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import InputRequired, Email, EqualTo, Length, ValidationError
+from models import User
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
+    username = StringField('Username', validators=[InputRequired()])
+    password = PasswordField('Password', validators=[InputRequired()])
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Log In')
+
+
+class SignUpForm(FlaskForm):
+    username = StringField('Username', validators=[InputRequired(),Length(min=5,max=20)])
+    email = StringField('Email', validators=[InputRequired(), Email('Please enter valid email')])
+    password = PasswordField("Password", validators=[InputRequired(),Length(min=6,max=20)])
+    password_repeat = PasswordField("Repeat Password", 
+        validators=[InputRequired(), EqualTo('password', message='Passwords must match')])
+    submit = SubmitField('Sign Up')
+
+    # custom validator following validate_<fieldname>
+    def validate_username(self,username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Please choose another username')
+
+    def validate_email(self,email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Account with this email already exists')
--- run.py
+++ run.py
@@ -1,8 +1,9 @@
-from flask import Flask, render_template, flash, redirect, request, abort
+from flask import Flask, render_template, url_for, flash, redirect, request, abort
 from config import Config
-import forms
+from forms import LoginForm, SignUpForm
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from werkzeug.urls import url_parse
 from models import db, User, login_manager
 from flask_login import current_user, login_user, logout_user, login_required
 
@@ -12,15 +13,34 @@ db.init_app(app)
 # migrate = Migrate(app, db)
 login_manager.init_app(app)
 
+#initialize database tables once
+def init():
+    with app.app_context():
+        db.create_all()
+
 @app.route('/index')
 @app.route('/')
 @login_required
 def index():
     return render_template('index.html')
 
-@app.route('/signup')
+@app.route('/signup', methods=['GET', 'POST'])
 def signup():
-    return render_template('signup.html')
+    # if a logged in user tries to go to /signup
+    # redirect the user to index
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+    signup_form = SignUpForm()
+    if signup_form.validate_on_submit():
+        user = User(username=signup_form.username.data, 
+            email=signup_form.email.data)
+        user.set_password(signup_form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash("Congratulations! You are now a registered user.")
+        return redirect(url_for('login'))
+
+    return render_template('signup.html', title='Sign Up', signup_form=signup_form)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -29,11 +49,11 @@ def login():
     if current_user.is_authenticated:
         return redirect(url_for('index'))
 
-    login_form = forms.LoginForm()
+    login_form = LoginForm()
     if login_form.validate_on_submit():
 
         user = User.query.filter_by(username=login_form.username.data).first()
-        if not user.check_password(login_form.password.data) or user is None:
+        if user is None or not user.check_password(login_form.password.data):
             flash("Incorrect Username or Password")
             return redirect(url_for('login'))
 
@@ -41,10 +61,10 @@ def login():
         flash('Logged in successfully.')
         next_page = request.args.get('next')
         # is_safe_url should check if the url is safe for redirects.
-        if not is_safe_url(next_page):
-            return abort(400, "Unsafe URL")
-        return redirect(url_for('/index'))
-    return render_template('login.html', login_form = login_form)
+        if not next_page or url_parse(next_page).netloc != '':
+            next_page = url_for('index')
+        return redirect(next_page)
+    return render_template('login.html', title='Login', login_form = login_form)
 
 @app.route('/logout')
 def logout():
@@ -53,4 +73,5 @@ def logout():
 
 
 if __name__ == '__main__':
+    #init()
     app.run()
